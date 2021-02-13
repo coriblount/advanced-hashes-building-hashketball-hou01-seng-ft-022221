15fe17a4bfddcf98cdefe17019c5ea53ed4bb394
cori <heycorihey@icloud.com>
2021-02-13 05:28:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,25 @@
+def game_hash {
+
+    :home=>{
+      :team_name: "Brooklyn Nets",
+      :colors=>["Black", "White"
+        ]
+      :players=>[{
+       } ]
+      }
+    }
+:away=>{
+  :team_name=>{"Charlotte Hornets"
+}
+:colors=>["Turqoise, Purple"
+  ]
+  :players=>[{
+    
+  }]
+  }
+
+end
+game_hash
+end
+}
 # Write your code here!
